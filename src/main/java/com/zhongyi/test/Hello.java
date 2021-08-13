e85ba9f27a175375c1dee0de94a158210e8886c5
zhongyi1115 <82957987+zhongyi1115@users.noreply.github.com>
2021-08-13 11:10:21 +0800

    Update Hello.java
    本地修改

--- src/main/java/com/zhongyi/test/Hello.java
+++ src/main/java/com/zhongyi/test/Hello.java
@@ -13,5 +13,6 @@ public class Hello {
         System.out.println("master git2!");
         System.out.println("hot git2!");
         System.out.println("hot git3!");
+        System.out.println("hot git4!");
     }
 }
