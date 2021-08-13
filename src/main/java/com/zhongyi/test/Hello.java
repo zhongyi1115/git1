d1b5929567012298c72becfc377e78261e846bc4
longtianyu <203289960@qq.com>
2021-08-13 09:44:28 +0800

    第一次提交

---
+++ src/main/java/com/zhongyi/test/Hello.java
@@ -0,0 +1,11 @@
+package com.zhongyi.test;
+
+/**
+ * @author shkstart
+ * @create 2021-08-13 9:41
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+    }
+}
