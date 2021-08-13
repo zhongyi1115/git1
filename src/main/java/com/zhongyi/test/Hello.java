b6da39f975cf0d85ba5865ce3cd93352f799f9a8
longtianyu <203289960@qq.com>
2021-08-13 09:46:07 +0800

    第二次提交

--- src/main/java/com/zhongyi/test/Hello.java
+++ src/main/java/com/zhongyi/test/Hello.java
@@ -7,5 +7,6 @@ package com.zhongyi.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello word!");
+        System.out.println("hello git!");
     }
 }
